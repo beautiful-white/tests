ba9dbebca4bb0d60d26a522601972c0904a0bb34
Roman Solovev <w.roman.developer@gmail.com>
2024-07-21 19:32:57 +0300

    modified second question

--- second_question/libForArea/__init__.py
+++ second_question/libForArea/__init__.py
@@ -11,10 +11,31 @@ class Area:
     def __init__(self) -> None:
         pass
 
+    def _triangle_check(self, a_side: float, b_side: float, c_side: float) -> None:
+        "Функция проверки верности введенных данных"
+
+        if type(a_side) not in [int, float] \
+                or type(b_side) not in [int, float] \
+                or type(c_side) not in [int, float]:
+            raise TypeError("Неверный тип данных")
+
+        if a_side < 0 or a_side < 0 or a_side < 0:
+            raise ValueError("Сторона меньше нуля")
+
+    def _circle_check(self, radius: float) -> None:
+        "Функция проверки верности введенных данных"
+
+        if type(radius) not in [int, float]:
+            raise TypeError("Неверный тип данных")
+
+        if radius < 0:
+            raise ValueError("Радиус меньше нуля")
+
     def _sqrt(self, number: float) -> float:
         "Функция вычисление корня"
 
-        assert number > 0, "Нельзя взять корень из отрицательного числа"
+        if number < 0:
+            raise ValueError("Нельзя взять корень из отрицательного числа")
 
         root = number ** 0.5
         return root
@@ -28,17 +49,31 @@ class Area:
     def circle(self, radius: float) -> float:
         "Функция вычисления площади круга через радиус"
 
+        self._circle_check(radius)
+
         area = pi * self._pow(radius, 2)
         return area
 
     def triangle(self, a_side: float, b_side: float, c_side: float) -> float:
         "Функция вычисления площади треугольника через формулу Герона"
 
+        self._triangle_check(a_side, b_side, c_side)
+
         half_meter = (a_side + b_side + c_side) / 2
         product = half_meter * (half_meter - a_side) * \
             (half_meter - b_side) * (half_meter - c_side)
 
-        assert product > 0, "Треугольник с заданными сторонами не существует"
+        if product < 0:
+            raise ValueError("Треугольник с такими сторонами не существует")
 
         area = self._sqrt(product)
         return area
+
+    def is_right_angled_triangle(self, a_side: float, b_side: float, c_side: float) -> bool:
+        "Функция для проверки треугольника на прямоугольность"
+
+        self._triangle_check(a_side, b_side, c_side)
+
+        sides = sorted([a_side, b_side, c_side])
+        squares = [side**2 for side in sides]
+        return squares[2] == squares[0] + squares[1]
--- second_question/test_areas.py
+++ second_question/test_areas.py
@@ -1,21 +1,32 @@
+import unittest
+from math import pi
 from libForArea import Area
 
 
-def test_circle_1():
-    obj = Area()
-    assert int(obj.circle(123)) == 47529, "Несовпадение ответа"
+class TestAreas(unittest.TestCase):
+    e = Area()
 
+    def test_area(self):
+        self.assertEqual(self.e.circle(12), 12 ** 2 * pi)
+        self.assertEqual(self.e.circle(123), 123 ** 2 * pi)
+        self.assertEqual(self.e.circle(1255213), 1255213 ** 2 * pi)
+        self.assertEqual(self.e.circle(0), 0)
+        self.assertEqual(self.e.circle(1), pi)
+        self.assertEqual(self.e.triangle(12, 10, 8),
+                         (15*(15-12)*(15-10)*(15-8))**.5)
+        self.assertEqual(self.e.triangle(17, 10, 9),
+                         (18*(18-17)*(18-10)*(18-9))**.5)
+        self.assertEqual(self.e.triangle(0, 0, 0), 0)
 
-def test_circle_2():
-    obj = Area()
-    assert int(obj.circle(15)) == 706, "Несовпадение ответа"
+    def test_values(self):
+        self.assertRaises(ValueError, self.e.circle, -1)
+        self.assertRaises(ValueError, self.e.circle, -10000)
+        self.assertRaises(ValueError, self.e.triangle, 10, 2, 3)
+        self.assertRaises(ValueError, self.e.triangle, -3, 10, 12)
 
-
-def test_triangle_1():
-    obj = Area()
-    assert int(obj.triangle(3, 4, 5)) == 6, "Несовпадение ответа"
-
-
-def test_triangle_2():
-    obj = Area()
-    assert int(obj.triangle(5, 6, 10)) == 11, "Несовпадение ответа"
+    def test_types(self):
+        self.assertRaises(TypeError, self.e.circle, [123, 123, 123])
+        self.assertRaises(TypeError, self.e.circle, "hello")
+        self.assertRaises(TypeError, self.e.triangle,
+                          [123, 123, 123], "wsf", 123)
+        self.assertRaises(TypeError, self.e.triangle, "hello", {123, 123}, 123)
